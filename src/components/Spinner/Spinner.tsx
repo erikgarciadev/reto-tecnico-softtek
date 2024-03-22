10c563c37c28043dc8a22bc78473e5100353b13e
Erik Garcia Tacas <erik.fisi16@gmail.com>
2024-03-21 23:15:10 -0500

    fix: add lazy to routes

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,7 @@
+import "./styles.scss";
+
+const Spinner = () => {
+  return <div className="spinner"></div>;
+};
+
+export default Spinner;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,56 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
 import { PATHS } from "../utils/constants";
-import Plans from "../pages/Plans";
-import Summary from "../pages/Summary";
+import Spinner from "../components/Spinner";
+const Home = React.lazy(() => import("../pages/Home"));
+const Plans = React.lazy(() => import("../pages/Plans"));
+const Summary = React.lazy(() => import("../pages/Summary"));
 
 const index = () => {
   return (
     <Routes>
-      <Route path={PATHS.HOME} element={<Home />} />
-      <Route path={PATHS.PLANS} element={<Plans />} />
-      <Route path={PATHS.SUMMARY} element={<Summary />} />
+      <Route
+        path={PATHS.HOME}
+        element={
+          <React.Suspense
+            fallback={
+              <div className="center-full-page">
+                <Spinner />
+              </div>
+            }
+          >
+            <Home />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path={PATHS.PLANS}
+        element={
+          <React.Suspense
+            fallback={
+              <div className="center-full-page">
+                <Spinner />
+              </div>
+            }
+          >
+            <Plans />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path={PATHS.SUMMARY}
+        element={
+          <React.Suspense
+            fallback={
+              <div className="center-full-page">
+                <Spinner />
+              </div>
+            }
+          >
+            <Summary />
+          </React.Suspense>
+        }
+      />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
